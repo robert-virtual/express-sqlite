001f4628750ff049e08ef6454ff10bce08333cb3
robert-virtual <robertocastill63@gmail.com>
2022-11-09 16:04:07 -0600

    remove dist

--- dist/libs/checkObj.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validObj = void 0;
-function validObj(obj) {
-    return Object.values(obj).filter((e) => e == undefined).length > 0;
-}
-exports.validObj = validObj;
